e6aba46e419f9c5b000536c6123593bfca7568d4
Julian Lork <118125250+julianlork@users.noreply.github.com>
2023-09-03 12:07:08 +0200

    Initial Commit

---
+++ gpu-vector-addition-with-metal-and-cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gpu-vector-addition-with-metal-and-cpp
+//
+//  Created by Julian Lork on 03.09.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
